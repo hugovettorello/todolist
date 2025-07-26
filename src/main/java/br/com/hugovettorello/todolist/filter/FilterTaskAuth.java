3306ca4cd5d904b11d1135570a85368ede0d624c
hugovettorello <hugovettorello13@gmail.com>
2025-07-25 21:48:40 -0300

    feat: ensure that user validation only takes place on  the path /tasks/

--- src/main/java/br/com/hugovettorello/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/hugovettorello/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.hugovettorello.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.hugovettorello.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,23 +18,47 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    
+    @Autowired
+    private IUserRepository userRepository;
+    
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
                 
-                var authorization = request.getHeader("Authorization");
+                var servletPath = request.getServletPath();
 
-                var authEncoded = authorization.substring("Basic".length()).trim();
+                if (servletPath.equals("/tasks/")) {
+                     var authorization = request.getHeader("Authorization");
 
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+                    var authEncoded = authorization.substring("Basic".length()).trim();
 
-                var authString = new String(authDecode);
+                    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
 
-                String[] credentials = authString.split(":");
-                String username = credentials[0];
-                String password = credentials[1];
+                    var authString = new String(authDecode);
 
-                filterChain.doFilter(request, response);
+                    String[] credentials = authString.split(":");
+                    String username = credentials[0];
+                    String password = credentials[1];
+
+                    var user = this.userRepository.findByUsername(username);
+                    if (user == null) {
+                        response.sendError(401);
+                    } else {
+
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+                        if(passwordVerify.verified){
+                            filterChain.doFilter(request, response);
+                        } else {
+                            response.sendError(401);
+                        }
+
+                    }
+                } else {
+                    filterChain.doFilter(request, response);
+                }
     }
 
 
